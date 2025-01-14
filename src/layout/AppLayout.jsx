a2af96353b3e9435f6eea0af19712af00116947b
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2025-01-14 19:13:18 +0100

    feat: complete milestone 1

---
+++ src/layout/AppLayout.jsx
@@ -0,0 +1,19 @@
+import Searchbar from "../components/Searchbar"
+import Home from "../pages/Home";
+
+function AppLayout() {
+    return (
+        <>
+            <header>
+                <Searchbar />
+            </header>
+            <main>
+                <section>
+                    <Home />
+                </section>
+            </main>
+        </>
+    )
+}
+
+export default AppLayout;
